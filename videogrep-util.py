245f925672a7188634e14354a92b8370d00f6b5f
Andrew Rhoads <andrew.c.rhoads@gmail.com>
2024-12-18 00:41:09 -0800

    add script

---
+++ videogrep-util.py
@@ -0,0 +1,160 @@
+import os
+import subprocess
+import importlib
+import re
+import argparse
+import mimetypes
+
+from videogrep import videogrep
+
+EPISODE_PATTERN = re.compile(r'(?P<episode>S\d{1,2}E\d{1,2})')
+
+EXTRA_COMMANDs = ['-map', '0:a:m:language:eng?']
+# copied from moviepy. We only want to add in the audio channel mapping
+def monkey_patched_moviepy(self, starttime = 0):
+    import numpy as np
+    from moviepy.compat import DEVNULL, PY3
+    from moviepy.config import get_setting
+    import subprocess as sp
+
+    """ Opens the file, creates the pipe. """
+
+    self.close_proc() # if any
+
+    if starttime !=0 :
+        offset = min(1,starttime)
+        i_arg = ["-ss", "%.05f"%(starttime-offset),
+                '-i', self.filename, '-vn',
+                "-ss", "%.05f"%offset]
+    else:
+        i_arg = [ '-i', self.filename,  '-vn']
+
+
+    cmd = ([get_setting("FFMPEG_BINARY")] + i_arg + EXTRA_COMMANDs +
+           [ '-loglevel', 'error',
+             '-f', self.f,
+            '-acodec', self.acodec,
+            '-ar', "%d"%self.fps,
+            '-ac', '%d'%self.nchannels, '-'])
+
+    popen_params = {"bufsize": self.buffersize,
+                    "stdout": sp.PIPE,
+                    "stderr": sp.PIPE,
+                    "stdin": DEVNULL}
+
+    if os.name == "nt":
+        popen_params["creationflags"] = 0x08000000
+
+    self.proc = sp.Popen( cmd, **popen_params)
+
+    self.pos = np.round(self.fps*starttime)
+
+def patch_moviepy():
+    import moviepy
+
+    if importlib.metadata.version('moviepy') == '1.0.3':
+        savedFunc = moviepy.audio.io.readers.FFMPEG_AudioReader.initialize
+        moviepy.audio.io.readers.FFMPEG_AudioReader.initialize = monkey_patched_moviepy
+    else:
+        print("unable to patch moviepy. 1.0.3 is supported only.")
+
+def prepare_video(fullpath, subtitleFile, toDelete):
+    wasCreated = False
+    if not os.path.isfile(subtitleFile):
+        assert subtitleFile[-4:] == '.srt', 'subtitleFile must end in srt'
+        wasCreated = True
+        command = [
+            "ffmpeg",
+            "-i", fullpath,
+            "-map", "0:s:0",  # Select subtitle stream
+            subtitleFile,
+            "-y"  # Overwrite output file if it exists
+        ]
+        subprocess.run(command, check=True)
+
+    toDelete.append((subtitleFile, wasCreated))
+
+def get_top_level_name(fullpath):
+    split_path = fullpath.split(str(os.sep))
+    
+    # Look for the "Season X" folder first
+    for i, component in enumerate(split_path):
+        if "Season" in component:
+            return split_path[i - 1]  # The folder before "Season X" is the show name
+
+    return split_path[-1]
+
+def process_file(args, file, toProcess, toDelete):
+    if file is None:
+        # os.path.join let's us be sloppy
+        file = args.input
+
+    filename = os.fsdecode(file)
+    if not mimetypes.guess_type(filename)[0].startswith('video'):
+        return
+
+    fullpath = os.path.join(args.input, filename)
+
+    withoutExtension, _ = os.path.splitext(filename)
+    subtitleFile = os.path.join(args.input, f'{withoutExtension}.srt')
+
+    prepare_video(fullpath, subtitleFile, toDelete)
+
+    if not args.combine:
+        outputName = get_top_level_name(args.input)
+        match = EPISODE_PATTERN.search(withoutExtension)
+        if match:
+            notFullPath = withoutExtension.split(str(os.sep))[-1]
+            outputName = f'{notFullPath[:match.end('episode')]} supercut.mp4'
+
+        videogrep(fullpath, args.search, output=outputName, search_type=args.search_type)#, padding=3)
+    else:
+        toProcess.append(fullpath)
+
+def process_files(args):
+    userInput = args.input
+    searchTerm = args.search
+
+    if not os.path.exists(userInput):
+        print("invalid input")
+        return
+
+    toProcess = []
+    toDelete = []
+
+    directory = os.fsencode(userInput)
+    
+    if os.path.isdir(directory):
+        for file in os.listdir(directory):
+            process_file(args, file, toProcess, toDelete)
+    else:
+        process_file(args, None, toProcess, toDelete)
+
+    if toProcess:
+        print(get_top_level_name(userInput))
+        videogrep(toProcess, searchTerm, output=f'{get_top_level_name(userInput)} {searchTerm} supercut.mp4', search_type=args.search_type)#, padding=3)
+
+    if args.delete_generated:
+        for file, wasCreated in toDelete:
+            if wasCreated or args.deleted_all_srt:
+                os.remove(file)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--search", help='search input')
+    parser.add_argument("-st", "--search-type", default="sentence")
+    parser.add_argument("-i", "--input")
+    parser.add_argument("-d", "--delete-generated", default=False, help='delete str generated this run')
+    parser.add_argument("-dall", "--delete-all-srt", default=False, help="delete srt even if not generated this run")
+    parser.add_argument("-c", "--combine", default=False, help='if true output is collated')
+    parser.add_argument("-force", "--force-english", default=True, help='if true enable hack to select English audio channel')
+
+    args = parser.parse_args()
+
+    if args.force_english:
+        patch_moviepy()
+
+    process_files(args)
+
+if __name__ == '__main__':
+    main()
